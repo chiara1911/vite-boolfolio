46c2ab78907e24496174b8ec45cfdac993f3b2a6
chiara1911 <chiaracesari89@gmail.com>
2024-01-19 17:48:51 +0100

    add projectComponent

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,32 @@
+<template>
+  <ProjectApp/>
+  <button @click="nextPage">avanti</button>
+  <button @click="previousPage">indietro</button>
+  
+  
+  </template>
+
+
+
 <script>
 
-import axios from "axios";
-import {store} from "/src/data/store.js";
+// import 
+import {store} from "./data/store";
+import ProjectApp from "./components/ProjectApp.vue";
 
 export default{
+ 
   name: 'App',
   components: {
-
-
+    ProjectApp,
   },
   data(){
-return{
-store,
-projects:[],
-currentPage: 1,
-lastPage: 0,
-
-};
-
-  },
-  methods:{
-    getAllProjects(){
-      axios.get(store.apiUrl + "/projects", {params:{page: this.currentPage}}).then((resp)=>{
-        console.log(resp.data);
-        this.projects = resp.data.results.data;
-        this.currentPage = resp.data.results.current_page;
-        this.lastPage = resp.data.results.last_page;
-      })
-    },
-    nextPage(){
-      this.currentPage +=1;
-      this.getAllProjects();
-
-    },
-    previousPage(){
-      this.currentPage -=1;
-      this.getAllProjects();
-
+    return{
+      store,          
     }
-  },
-  mounted() {
-    this.getAllProjects();
+  }, 
   }
-}
 </script>
-
-<template>
-<ul>
-  <li v-for="project in projects" :key="project.slug">
-    <div>
-<p> {{ project.title }}</p>
-<p> {{ project.description }}</p>
-
-<a href="#">{{ project.link }}</a>
-    </div>
-   
-  </li>
-
-</ul>
-<button @click="nextPage">avanti</button>
-<button @click="previousPage">indietro</button>
-
-
-</template>
-
-
 <style scoped lang="scss">
 
 </style>
---
+++ src/components/ProjectApp.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <ul class="card">
+      <li v-for="project in projects" :key="project.slug" class="card-header">
+        <div class="card-body">
+          <p>{{ project.title }}</p>
+          <p>{{ project.description }}</p>
+          <p>{{ project.technologies }}</p>
+          <a href="#">{{ project.link }}</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import  {store} from "../data/store";
+import axios from "axios";
+export default {
+  name: "ProjectApp",
+  data() {
+    return {
+      store,
+      projects: [],
+      currentPage: 1,
+      lastPage: 0,
+    };
+  },
+  methods:{
+    getAllProjects(){
+      axios.get(store.apiUrl + "/projects", {params:{page: this.currentPage}}).then((resp)=>{
+        console.log(resp.data);
+        this.projects = resp.data.results.data;
+        this.currentPage = resp.data.results.current_page;
+        this.lastPage = resp.data.results.last_page;
+      })
+    },
+    nextPage(){
+      this.currentPage +=1;
+      this.getAllProjects();
+
+    },
+    previousPage(){
+      this.currentPage -=1;
+      this.getAllProjects();
+
+    }
+  },
+  mounted() {
+    this.getAllProjects();
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
