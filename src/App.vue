cae162885dc90effa913dd8da4348a6107516a30
chiara1911 <chiaracesari89@gmail.com>
2024-01-19 23:52:53 +0100

    add category component in main view

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 <main>
   <ProjectApp/>
 <TechComponent/>
+<CategoryComponent/>
 </main>
 
 
@@ -19,6 +20,8 @@ import {store} from "./data/store";
 import ProjectApp from "./components/main/ProjectApp.vue";
 import HeaderComponent from "./components/header/HeaderComponent.vue";
 import TechComponent from "./components/main/TechComponent.vue";
+import CategoryComponent from "./components/main/CategoryComponent.vue";
+
 
 
 export default{
@@ -28,6 +31,7 @@ export default{
     ProjectApp,
     HeaderComponent,
     TechComponent,
+    CategoryComponent,
  
 },
   data(){
---
+++ src/components/main/CategoryComponent.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <ul class="card mt-4">
+      <h3 class="card-head">Categorie</h3>
+      <li
+        v-for="category in categories"
+        :key="category.slug"
+        class="card-header"
+      >
+        <div class="card-body">
+          <a href="#"
+            ><p>{{ category.name }}</p></a
+          >
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { store } from "../../data/store";
+import axios from "axios";
+export default {
+  name: "CategoryComponent",
+  data() {
+    return {
+      store,
+      categories: [],
+    };
+  },
+  methods: {
+    getAllCategories() {
+      axios.get(store.apiUrl + "/categories").then((resp) => {
+        console.log(resp.data);
+        this.categories = resp.data.results.data;
+        console.log(resp.data.results.data);
+      });
+    },
+  },
+  mounted() {
+    this.getAllCategories();
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
