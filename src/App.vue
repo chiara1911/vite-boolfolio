091591e334995252ee7f62be4c7e88e5b3f4b0c2
chiara1911 <chiaracesari89@gmail.com>
2024-01-19 16:46:22 +0100

    create vue project and add route to laravel-boolfolio

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<script>
+
+import axios from "axios";
+import {store} from "/src/data/store.js";
+
+export default{
+  name: 'App',
+  components: {
+
+
+  },
+  data(){
+return{
+store,
+projects:[],
+currentPage: 1,
+lastPage: 0,
+
+};
+
+  },
+  methods:{
+    getAllProjects(){
+      axios.get(store.apiUrl + "/projects", {params:{page: this.currentPage}}).then((resp)=>{
+        console.log(resp.data);
+        this.projects = resp.data.results.data;
+        this.currentPage = resp.data.results.current_page;
+        this.lastPage = resp.data.results.last_page;
+      })
+    },
+    nextPage(){
+      this.currentPage +=1;
+      this.getAllProjects();
+
+    },
+    previousPage(){
+      this.currentPage -=1;
+      this.getAllProjects();
+
+    }
+  },
+  mounted() {
+    this.getAllProjects();
+  }
+}
+</script>
+
+<template>
+<ul>
+  <li v-for="project in projects" :key="project.id">
+    {{ project.title }}
+  </li>
+
+</ul>
+<button @click="nextPage">avanti</button>
+<button @click="previousPage">indietro</button>
+
+
+</template>
+
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/data/store.js
@@ -0,0 +1,7 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+  apiUrl: "http://localhost:8000/api",
+  
+
+});
